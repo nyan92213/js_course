907a53a8a4798c41935c5f46d6da6239fdf810f6
nyan92213 <ivanbezrukov92213@gmail.com>
2023-02-18 14:58:51 +0300

    completed first task

--- first_project/script.js
+++ first_project/script.js
@@ -1,12 +1,25 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?");
+const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", '');
 
-const obj = {
+const personalMovieDatabase = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
+
+const a = prompt('Один из последних просмотренных фильмов', ''),
+      b = prompt('Какуюоценку поставите этому фильму?', ''),
+      c = prompt('Один из последних просмотренных фильмов', ''),
+      d = prompt('Какуюоценку поставите этому фильму?', '');
+
+personalMovieDatabase.movies[a] = b;
+personalMovieDatabase.movies[c] = d;
+
+console.log(personalMovieDatabase);
+
+
+
 
