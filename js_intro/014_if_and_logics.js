9efd6ee038ee2990ff51c0ab302b4114a6325b64
nyan92213 <ivanbezrukov92213@gmail.com>
2023-02-18 16:12:42 +0300

    cycles added

--- js_intro/014_if_and_logics.js
+++ js_intro/014_if_and_logics.js
@@ -51,15 +51,47 @@ switch (num) {
 // console.log(null && 5);
 // console.log(0 && 'sdjhb')
 
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+//     console.log('Все довольны');
+// } else {
+//     console.log('Кто-то голоден');
+// };
+
+console.log(!0);
+
+console.log( NaN || 2 || undefined );
+
+
+console.log( NaN && 2 && undefined );
+
+
+console.log( 1 && 2 && 3 );
+
+
+console.log( !1 && 2 || !3 );
+
+
+console.log( 25 || null && !3 );
+
+
+console.log( NaN || null || !3 || undefined || 5);
+
+
+console.log( NaN || null && !3 && undefined || 5);
+
+
+console.log( 5 === 5 && 3 > 1 || 5);
+
 const hamburger = 3;
 const fries = 3;
 const cola = 0;
 const nuggets = 2;
 
-if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-    console.log('Все довольны');
-} else {
-    console.log('Кто-то голоден');
-};
-
-console.log(!0);
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
