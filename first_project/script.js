274fd875cd5c6e4941ffde8f422fa7a7ad6d63af
nyan92213 <ivanbezrukov92213@gmail.com>
2023-02-18 14:45:34 +0300

    added first project

---
+++ first_project/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?");
+
+const obj = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
