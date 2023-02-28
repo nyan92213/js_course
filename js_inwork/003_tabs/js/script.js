0e8cd4b7b46e9488280491ca0ba52783d1eec213
nyan92213 <ivanbezrukov92213@gmail.com>
2023-02-28 17:26:52 +0300

    updated timer uncluding incorrect datetime of start

--- js_inwork/003_tabs/js/script.js
+++ js_inwork/003_tabs/js/script.js
@@ -38,14 +38,22 @@
     })
     // работа с обратным таймером акции
 
-    const deadline = '2023-03-11'
+    const deadline = '2022-03-11'
 
     function getTimeRemaining(endtime = '2023-03-11') {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-              days = Math.floor(t/(1000 * 60 * 60 * 24)),
-              hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-              minutes = Math.floor((t / (1000 * 60)) % 60),
-              seconds = Math.floor((t / 1000) % 60)
+        let days, hours, minutes, seconds
+        const t = Date.parse(endtime) - Date.parse(new Date())
+        if (t <= 0) {
+            days = 0
+            hours = 0
+            minutes = 0
+            seconds = 0
+        } else {
+            days = Math.floor(t/(1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((t / (1000 * 60)) % 60),
+            seconds = Math.floor((t / 1000) % 60)
+        }
         return {
             'total': t,
             'days': days,
